e38e1b4102590ec86e4e89eb38e32326f88f1463
Brian Langay <67788456+brianlangay4@users.noreply.github.com>
2022-09-03 21:06:49 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -12,17 +12,20 @@ public class Main {
         try {
             //used connection to obtain the sql connection
             //used a DriverManager  for our sql connection from project lib dependency
+            //replace "toor" to your own password for the database
             Connection dbs = DriverManager.getConnection("jdbc:mysql://root@127.0.0.1:3306/user", "root", "toor");
            //Statement used for the connection
             Statement statement = dbs.createStatement();
             //Result set  to obtain the query in our schema
+            //most important class to use get to use the sql language to make changes to our database or print results 
             ResultSet resultSet = statement.executeQuery("select * from logs");
             //looped to obtain all the items in the table selected
+            //this loop is quite important to get all the information of the selected database example in this case was a table 
             while (resultSet.next()) {
                 System.out.println(resultSet.getString("name"));
             }
         }
-        //last e catch the Exception to complete the code
+        //last catch the Exception to complete the code
         catch (Exception e){
             e.printStackTrace();
         }
